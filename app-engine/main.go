b06359cbafcf84c212d6caf2ca9da96746153da1
jrudio <justinjrudio@gmail.com>
2021-06-25 01:31:44 -0700

    initial app engine setup

--- app-engine/main.go
+++ app-engine/main.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	language "cloud.google.com/go/language/apiv1"
@@ -13,10 +17,6 @@ import (
 )
 
 func main() {
-	// listen to rube_goldberg pubsub topic
-
-	// listen for event type and filename to parse
-
 	// download reddit posts from json on cloud storage
 
 	// run posts through entity/sentiment api while abiding
@@ -24,73 +24,133 @@ func main() {
 
 	// send results to cloud storage
 
-	// signal we are finished once we save posts to cloud storage
+	// signal we are finished once we have saved posts to cloud storage
+
+	http.HandleFunc("/api/analyze/posts", analyzePostHandler)
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func appendToFilename(filename string, addendum string) string {
+	extension := filepath.Ext(filename)
+
+	return strings.Replace(filename, extension, "_", 1) + addendum + extension
+}
+
+func analyzePostHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("must be GET request"))
+
+		return
+	}
 
 	projectBucket := "rube_goldberg_project"
-	outputFilename := "reddit_data"
+	subBucket := "reddit_data"
+
+	query := r.URL.Query()
+
+	// this file must live within cloud storage
+	filename := query.Get("filename")
+	outputFilename := appendToFilename(filename, "analyzed")
+
+	if filename == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("missing required input filename"))
+
+		return
+	}
 
 	ctx := context.Background()
 
 	languageClient, err := language.NewClient(ctx)
 
 	if err != nil {
-		fmt.Printf("failed to create language client: %v\n", err)
+		log.Printf("failed to create language client: %v\n", err)
 
-		os.Exit(1)
+		return
 	}
 
 	storageClient, err := storage.NewClient(ctx)
 
 	if err != nil {
-		fmt.Printf("failed to create storage client: %v\n", err)
+		log.Printf("failed to create storage client: %v\n", err)
 
-		os.Exit(1)
+		return
 	}
 
 	defer (func(languageClient *language.Client, storageClient *storage.Client) {
 		languageClient.Close()
 		storageClient.Close()
 	})(languageClient, storageClient)
-	// return
 
-	sampleComment := []sentiment.RedditPost{
-		{
-			ID: "o74ykd",
-			Body: `We have traditional CRUD app with frontend and backend parts, written in React and Express (NodeJS), respectively. We serve both of them on Google App Engine as two separate services. The issue is that we get unbelievably high monthly costs for serving frontend app on GAE, whereas $0 for serving backend app. I am totally frustrated because serving a standard React app (which is built to static app) on alternative platforms (e.g. Netlify) is free. Is there any way to completely disable scaling stuff and run this app on GAE for free?
-
-			P.S. I am sorry if it is a dumb question but I couldn't figure it out on my own.
-			`,
-		},
-	}
-
-	fmt.Println("analyzing posts")
-
-	analyzedPosts, err := sentiment.AnalyzeEntitesInPosts(ctx, languageClient, sampleComment)
-
-	if err != nil {
-		fmt.Printf("analysis failed: %v\n", err)
-
-		os.Exit(1)
-	}
-
-	// save to cloud storage
+	// pull posts from cloud storage
 	storageCTX, storageCTXCancel := context.WithTimeout(ctx, time.Second*50)
 
 	defer storageCTXCancel()
 
-	storageWriter := storageClient.Bucket(projectBucket).Object(outputFilename).NewWriter(storageCTX)
+	var posts []sentiment.RedditPost
+
+	storageReader, err := storageClient.Bucket(projectBucket + "/" + subBucket).Object(filename).NewReader(storageCTX)
+
+	if err != nil {
+		log.Printf("getting bucket reader failed: %v\n", err)
+
+		return
+	}
+
+	defer storageReader.Close()
+
+	if err := json.NewDecoder(storageReader).Decode(&posts); err != nil {
+		log.Printf("parsing json failed: %v\n", err)
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprintf(w, "analyzing %d posts", len(posts))
+	log.Printf("analyzing %d posts\n", len(posts))
+
+	// w.Write([]byte(fmt.Sprintf("go to /api/progress?id=%d to check on this requests progress", 123)))
+
+	analyzedPosts, err := sentiment.AnalyzeEntitesInPosts(ctx, languageClient, posts)
+
+	if err != nil {
+		log.Printf("analysis failed: %v\n", err)
+
+		return
+	}
+
+	log.Printf("after pruning posts with empty body we analyzed %d posts\n", len(analyzedPosts))
+
+	// save to cloud storage
+	storageCTX, storageCTXCancel = context.WithTimeout(ctx, time.Second*50)
+
+	defer storageCTXCancel()
+
+	storageWriter := storageClient.Bucket(projectBucket).Object(subBucket + "/" + outputFilename).NewWriter(storageCTX)
 
 	defer storageWriter.Close()
 
 	if err := json.NewEncoder(storageWriter).Encode(analyzedPosts); err != nil {
-		fmt.Printf("failed to upload analyzed posts: %v\n", err)
+		log.Printf("failed to upload analyzed posts: %v\n", err)
 
-		os.Exit(1)
+		return
 	}
 
-	fmt.Printf("uploaded analyzed posts to '%s'\n", projectBucket+"/"+outputFilename)
+	log.Printf("uploaded analyzed posts to '%s'\n", projectBucket+"/"+outputFilename)
+
+	// go signalFinished()
 }
-
-// func analyzePostHandler(w http.ResponseWriter, r *http.Request) {
-
-// }
